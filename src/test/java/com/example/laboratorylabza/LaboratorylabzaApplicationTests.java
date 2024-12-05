be3f0da862334b17b640ad5eed9c296dd6355e34
Altynai Serikovna <altusha21072005@gmail.com>
2024-12-05 23:51:43 +0500

    Initial commit

---
+++ src/test/java/com/example/laboratorylabza/LaboratorylabzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.laboratorylabza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorylabzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
